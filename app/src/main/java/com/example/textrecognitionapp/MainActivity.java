6b148484ab2b2c7f405031d7c7b573e5eb26dc42
Saim <saimh355@gmail.com>
2019-12-18 17:17:21 +0500

    Android Text Recognition from Camera Using MLKit

--- app/src/main/java/com/example/textrecognitionapp/MainActivity.java
+++ app/src/main/java/com/example/textrecognitionapp/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.textrecognitionapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button captureImageBtn,detectTextBtn;
+    private ImageView imageView;
+    private TextView textView;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        captureImageBtn = findViewById(R.id.capture_image);
+        detectTextBtn = findViewById(R.id.detect_text_image);
+        imageView = findViewById(R.id.image_view);
+        textView = findViewById(R.id.txt_display);
+
+        captureImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 }
